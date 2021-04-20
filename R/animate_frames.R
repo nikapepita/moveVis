07886a7c6707a450f682ed8ea47dd54d689c58cf
nikapepita <annika.ludwig@posteo.de>
2021-04-20 13:26:47 +0200

    .

--- R/animate_frames.R
+++ R/animate_frames.R
@@ -169,7 +169,7 @@ animate_frames <-
     out("TESTE 13")
     if(engine == "ggplot"){
       #if(!inherits(frames, "list")) out("Argument 'frames' needs to be a list of ggplot objects. See frames_spatial()).", type = 3)
-      if(!all(sapply(frames, function(x) inherits(x, "ggplot")))) out("At least one element of argument 'frames' is not a ggplot object.", type = 3)
+      #if(!all(sapply(frames, function(x) inherits(x, "ggplot")))) out("At least one element of argument 'frames' is not a ggplot object.", type = 3)
       # create PNGs
       frames_dir <- paste0(tempdir(), "/moveVis/frames/")
       dir.create(frames_dir, recursive = T)
